a1812a92d2f9c495fdf2480129d5ebe922ddaf80
Kirill Yerin <kyrylo.yerin@repairfix.eu>
2022-07-23 22:35:24 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hahaha learn react I did that!!!99
+          Hahaha learn react
         </a>
       </header>
     </div>
