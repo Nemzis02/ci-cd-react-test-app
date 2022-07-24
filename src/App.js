1af5d10118ebe7e0fea5c1c3b9747a6b4c2d87e7
Kirill Yerin <kyrylo.yerin@repairfix.eu>
2022-07-24 22:30:45 +0300

    Test flow PR

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello learn react
+          Test flow 3 learn react
         </a>
       </header>
     </div>
