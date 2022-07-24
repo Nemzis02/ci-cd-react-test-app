d6f2f9f8e9200e2abd9bf5a496a70b2ee5fa341d
Kirill Yerin <kyrylo.yerin@repairfix.eu>
2022-07-24 21:25:27 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Yerin learn react
+          Yerin Alexander learn react
         </a>
       </header>
     </div>
