de777faac8f6b8c14919c1a6eb3dc000db3309ed
Kirill Yerin <kyrylo.yerin@repairfix.eu>
2022-07-24 22:14:31 +0300

    Break pipeline

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Pipe learn react
+          Pipe learn 1react
         </a>
       </header>
     </div>
