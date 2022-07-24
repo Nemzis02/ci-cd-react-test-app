37756892cbf4497f7fbefdd97f805714548ac631
Kirill Yerin <34840226+Nemzis02@users.noreply.github.com>
2022-07-24 22:03:54 +0300

    Pipeline test (#66)
    * Pipeline test
    
    * Pipeline test
    
    * Pipeline test
    
    * Pipeline test
    
    Co-authored-by: Kirill Yerin <kyrylo.yerin@repairfix.eu>

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Yerin Alexander learn react
+          Pipe learn react
         </a>
       </header>
     </div>
